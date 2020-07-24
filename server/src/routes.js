364c406ca5a17493d13b918802a509d1ed0747f0
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2020-07-24 14:28:35 -0300

    Adds route to download the csv

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const routes = Router()
+const FilesExport = require('./controllers/FilesExportController')
 
-routes.get('/downloads/csv')
+routes.get('/downloads/csv', FilesExport.downloadCsv)
 
 module.exports = routes
